25e7a34ebb2f40e87937db7184ab2aa6308cdf58
jdfrnco <katleenftorres@gmail.com>
2026-06-17 10:52:21 +0800

    Commit 2
    add loop

--- BSIT31E1_PRELIM_A2_TORRES_KATLEEN_JADE/Program.cs
+++ BSIT31E1_PRELIM_A2_TORRES_KATLEEN_JADE/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!"); 
+﻿while (true)
+{
+    Console.Write("Enter first number (or 'exit' to quit): ");
+    string input = Console.ReadLine();
+    if (input.ToLower() == "exit") break;
+
+    double num1 = double.Parse(input);
+
+    Console.Write("Enter second number: ");
+    double num2 = double.Parse(Console.ReadLine());
+
+    Console.Write("Enter operator (+, -, *, /): ");
+    string op = Console.ReadLine();
+
+    double result = 0;
+}
