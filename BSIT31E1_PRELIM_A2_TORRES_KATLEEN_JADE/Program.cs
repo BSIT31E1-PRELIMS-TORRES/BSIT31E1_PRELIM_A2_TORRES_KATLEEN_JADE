b01354a1682f9ddfef6f834223c354c5012cb809
jdfrnco <katleenftorres@gmail.com>
2026-06-17 10:52:55 +0800

    Commit 3
    add calculator operations

--- BSIT31E1_PRELIM_A2_TORRES_KATLEEN_JADE/Program.cs
+++ BSIT31E1_PRELIM_A2_TORRES_KATLEEN_JADE/Program.cs
@@ -2,6 +2,7 @@
 {
     Console.Write("Enter first number (or 'exit' to quit): ");
     string input = Console.ReadLine();
+
     if (input.ToLower() == "exit") break;
 
     double num1 = double.Parse(input);
@@ -13,4 +14,28 @@
     string op = Console.ReadLine();
 
     double result = 0;
+
+    if (op == "+")
+        result = num1 + num2;
+    else if (op == "-")
+        result = num1 - num2;
+    else if (op == "*")
+        result = num1 * num2;
+    else if (op == "/")
+    {
+        if (num2 == 0)
+        {
+            Console.WriteLine("Error: Cannot divide by zero.");
+            continue;
+        }
+        result = num1 / num2;
+    }
+    else
+    {
+        Console.WriteLine("Invalid operator. Please use +, -, *, /");
+        continue;
+    }
+
+    Console.WriteLine("Result: " + result);
+    Console.WriteLine();
 }
